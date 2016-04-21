c675e62c1640d43bc7a00f0ccfa51cef406e3089
Julian Sauer <julian_sauer@gmx.net>
2016-04-21 16:20:23 +0200

    Added recipes for crafting narcotics and tranquilizer arrows.

---
+++ src/main/java/de/gmx/endermansend/tameableCreatures/items/CustomRecipes.java
@@ -0,0 +1,33 @@
+package de.gmx.endermansend.tameableCreatures.items;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class CustomRecipes {
+
+    public void setUpNarcotics() {
+        ItemStack narcotics = new ItemStack(Material.POTION);
+        ItemMeta narcoticsMeta = narcotics.getItemMeta();
+        narcoticsMeta.setDisplayName("Narcotics");
+        narcotics.setItemMeta(narcoticsMeta);
+
+        ShapelessRecipe recipe = new ShapelessRecipe(narcotics);
+        recipe.addIngredient(Material.ROTTEN_FLESH);
+        recipe.addIngredient(Material.POISONOUS_POTATO);
+        Bukkit.addRecipe(recipe);
+    }
+
+    public void setUpTranquilizerArrow() {
+        ItemStack tranquilizerArrow = new ItemStack(Material.ARROW);
+        ItemMeta tranquilizerArrowMeta = tranquilizerArrow.getItemMeta();
+        tranquilizerArrowMeta.setDisplayName("Tranquilizer Arrow");
+        tranquilizerArrow.setItemMeta(tranquilizerArrowMeta);
+
+        ShapelessRecipe recipe = new ShapelessRecipe(tranquilizerArrow);
+        recipe.addIngredient(Material.ARROW);
+    }
+
+}
--- src/main/java/de/gmx/endermansend/tameableCreatures/main/TameableCreatures.java
+++ src/main/java/de/gmx/endermansend/tameableCreatures/main/TameableCreatures.java
@@ -1,6 +1,8 @@
 package de.gmx.endermansend.tameableCreatures.main;
 
 import de.gmx.endermansend.tameableCreatures.entities.CustomEntities;
+import de.gmx.endermansend.tameableCreatures.items.CustomRecipes;
+import de.gmx.endermansend.tameableCreatures.listeners.CraftItemListener;
 import de.gmx.endermansend.tameableCreatures.listeners.PlayerInteractListener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,8 +14,13 @@ public class TameableCreatures extends JavaPlugin {
 
         CustomEntities.TameableEntityType.registerCustomEntities();
 
+        CustomRecipes customRecipes = new CustomRecipes();
+        customRecipes.setUpNarcotics();
+        customRecipes.setUpTranquilizerArrow();
+
         PluginManager pluginManager = getServer().getPluginManager();
         pluginManager.registerEvents(new PlayerInteractListener(), this);
+        pluginManager.registerEvents(new CraftItemListener(), this);
 
         getLogger().info("Enabled");
 
