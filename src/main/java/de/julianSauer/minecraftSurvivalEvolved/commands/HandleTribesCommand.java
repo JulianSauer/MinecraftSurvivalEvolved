e49ddfc627eca078f5993218c9948b98967735bf
Julian Sauer <julian_sauer@gmx.net>
2017-05-14 04:09:53 +0200

    Simplified printing of multiple pages to chat.

--- src/main/java/de/julianSauer/minecraftSurvivalEvolved/commands/HandleTribesCommand.java
+++ src/main/java/de/julianSauer/minecraftSurvivalEvolved/commands/HandleTribesCommand.java
@@ -4,7 +4,7 @@ import de.julianSauer.minecraftSurvivalEvolved.messages.ChatMessages;
 import de.julianSauer.minecraftSurvivalEvolved.tribes.Tribe;
 import org.bukkit.command.CommandSender;
 
-import java.util.*;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 /**
@@ -92,52 +92,7 @@ public class HandleTribesCommand extends CommandHandler {
             return;
         }
 
-        int pageCount = (int) Math.ceil((double) tribes.size() / 10.0);
-        if (page > pageCount) {
-            sender.sendMessage(ChatMessages.ERROR_PAGE_DOESNT_EXIST.setParams("" + page));
-            return;
-        }
-
-        sender.sendMessage(ChatMessages.PAGE_COUNT.setParams(
-                "" + (page),
-                "" + pageCount)
-        );
-
-        List<String> tribeNames = splitIntoPages(
-                tribes.stream().map(Tribe::getName).collect(Collectors.toList()), 10)
-                .get(page - 1);
-
-        for (String tribeName : tribeNames)
-            sender.sendMessage("- " + tribeName);
-
-    }
-
-    /**
-     * Joins list entries to pages.
-     *
-     * @param entryList      Text that should be processed
-     * @param entriesPerPage Defines the number of entries from the list on each page
-     * @return Collection that maps page numbers to text
-     */
-    private Map<Integer, List<String>> splitIntoPages(List<String> entryList, int entriesPerPage) {
-
-        entryList = entryList.stream().sorted().collect(Collectors.toList());
-        Map<Integer, List<String>> pagedMap = new HashMap<>();
-        int size = entryList.size();
-        int pages = (int) Math.ceil((double) size / (double) entriesPerPage);
-
-        if (size <= entriesPerPage) {
-            pagedMap.put(0, entryList);
-            return pagedMap;
-        }
-
-        for (int i = 1; i <= pages; i++) {
-            List<String> pageList = new ArrayList<>();
-            for (int j = 0; j < entriesPerPage && j * i < size; j++)
-                pageList.add(entryList.get(i * j));
-            pagedMap.put(i - 1, pageList);
-        }
-        return pagedMap;
+        printPage(sender, tribes.stream().map(Tribe::getName).collect(Collectors.toList()), page, 10);
 
     }
 
